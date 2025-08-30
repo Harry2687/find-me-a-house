a593d4574bb7a57452588a3b600eb33210753e24
Harry2687 <harry.zhong2687@gmail.com>
2025-08-30 18:02:37 +0800

    Refactor property analyser to include commute time and nearby POIs
    This commit refactors the property analyser to include commute time to work and nearby points of interest (POIs).
    
    The changes include:
    *   Adding commute time calculation using Google Maps Directions API.
    *   Adding nearby POI search using Google Maps Places API.
    *   Displaying commute time and nearby POIs on a map.
    *   Updating the marimo version.

--- property_analyser.py
+++ property_analyser.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.15.0"
+__generated_with = "0.15.2"
 app = marimo.App(width="medium")
 
 
@@ -12,30 +12,28 @@ def _():
     import os
     import polars as pl
     import folium
+    import datetime
 
     load_dotenv()
 
     gmaps = googlemaps.Client(key=os.getenv("GMAPS_API_KEY"))
-    return folium, gmaps, mo, os, pl
+    return datetime, folium, gmaps, mo, os, pl
 
 
 @app.cell
 def _(mo, os):
     # Define Marimo input objects
     address = mo.ui.text_area(value=os.getenv("MY_ADDRESS"), placeholder="Enter property address...")
-    search_term = mo.ui.text(value="coles or woolworths or aldi")
-    travel_mode = mo.ui.dropdown(options=["walking", "driving", "transit"], value="walking")
-    return address, search_term, travel_mode
+    workplace = mo.ui.text_area(value=os.getenv("WORK"), placeholder="Enter work address...")
+    return address, workplace
 
 
 @app.cell
-def _(address, mo, search_term, travel_mode):
+def _(address, mo, workplace):
     mo.md(
         f"""
-    # Inputs
     Address: {address}\n
-    Search: {search_term}\n
-    Travel mode: {travel_mode}
+    Work Address: {workplace}
     """
     )
     return
@@ -44,11 +42,7 @@ def _(address, mo, search_term, travel_mode):
 @app.cell
 def _(address, gmaps):
     geocode_result = gmaps.geocode(address.value)
-    return (geocode_result,)
 
-
-@app.cell
-def _(geocode_result):
     location = geocode_result[0]["geometry"]["location"]
     origin_lat = location["lat"]
     origin_lng = location["lng"]
@@ -56,17 +50,77 @@ def _(geocode_result):
 
     formatted_address = geocode_result[0]["formatted_address"]
     location_id = geocode_result[0]["place_id"]
-    return formatted_address, origin, origin_lat, origin_lng
+    return formatted_address, origin_lat, origin_lng
 
 
 @app.cell
-def _(formatted_address, mo):
-    mo.md(f"""Address search result: {formatted_address}""")
+def _(gmaps, next_weekday_at_830, origin_lat, origin_lng, workplace):
+    work_directions = gmaps.directions(origin=(origin_lat, origin_lng), destination=workplace.value, mode="transit", arrival_time=next_weekday_at_830())
+    duration = work_directions[0]["legs"][0]["duration"]["text"]
+    distance = work_directions[0]["legs"][0]["distance"]["text"]
+    return distance, duration
+
+
+@app.cell
+def _(distance, duration, formatted_address, mo):
+    mo.md(
+        f"""
+    Address search result: {formatted_address}\n
+    It will take {duration} to travel {distance} to work.
+    """
+    )
     return
 
 
 @app.cell
-def _(gmaps, origin, pl, search_term):
+def _(
+    folium,
+    formatted_address,
+    get_nearby,
+    mo,
+    origin_lat,
+    origin_lng,
+    plot_nearby,
+):
+    map = folium.Map(location=[origin_lat, origin_lng], zoom_start=15)
+    folium.Marker(
+        [origin_lat, origin_lng],
+        tooltip=formatted_address,
+        icon=folium.Icon(icon="home"),
+    ).add_to(map)
+    plot_nearby(
+        map,
+        get_nearby((origin_lat, origin_lng), "woolworths or coles or aldi"),
+        icon=folium.Icon(
+            color="green", icon="basket-shopping", prefix="fa", icon_color="white"
+        ),
+    )
+    plot_nearby(
+        map,
+        get_nearby((origin_lat, origin_lng), "gym"),
+        icon=folium.Icon(
+            color="red", icon="dumbbell", prefix="fa", icon_color="white"
+        ),
+    )
+
+    mo.Html(map._repr_html_())
+    return
+
+
+@app.cell
+def _(datetime):
+    def next_weekday_at_830():
+        date = datetime.date.today()
+
+        while date.weekday() > 4:
+            date += datetime.timedelta(days=1)
+    
+        return datetime.datetime.combine(date, datetime.time(8, 30))
+    return (next_weekday_at_830,)
+
+
+@app.cell
+def _(gmaps, pl):
     def get_nearby(origin: tuple, search_term: str):
         places_nearby_result = gmaps.places_nearby(
             location=origin, keyword=search_term, rank_by="distance"
@@ -128,36 +182,22 @@ def _(gmaps, origin, pl, search_term):
         )
 
         return output_df
-
-    get_nearby(origin=origin, search_term=search_term.value)
     return (get_nearby,)
 
 
 @app.cell
-def _(
-    folium,
-    formatted_address,
-    get_nearby,
-    mo,
-    origin,
-    origin_lat,
-    origin_lng,
-    search_term,
-):
-    poi = get_nearby(origin=origin, search_term=search_term.value)
+def _(folium, pl):
+    def plot_nearby(map: folium.Map, nearby_points: pl.DataFrame, icon: folium.Icon):
+        for row in nearby_points.iter_rows(named=True):
+            folium.Marker(
+                [row["lat"], row["lng"]], 
+                tooltip=row["Name"], 
+                popup=f"Walking Duration: {row["Walking Duration"]}\nDriving Duration: {row["Driving Duration"]}",
+                icon=icon
+            ).add_to(map)
 
-    map = folium.Map(location=[origin_lat, origin_lng], zoom_start=15)
-    folium.Marker([origin_lat, origin_lng], tooltip=formatted_address, icon=folium.Icon(icon="home")).add_to(map)
-
-    for row in poi.iter_rows(named=True):
-        folium.Marker(
-            [row["lat"], row["lng"]], 
-            tooltip=row["Name"], 
-            popup=f"Walking Duration: {row["Walking Duration"]}\nDriving Duration: {row["Driving Duration"]}"
-        ).add_to(map)
-
-    mo.Html(map._repr_html_())
-    return
+        return None
+    return (plot_nearby,)
 
 
 if __name__ == "__main__":
